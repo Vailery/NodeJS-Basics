b454b94e3c3a64cbc519463f26c68a86f160686c
Vailery <leraliabik8@gmail.com>
2022-05-12 15:02:36 +0300

    feat: add first section

---
+++ nodeJS-basics/line.js
@@ -0,0 +1,8 @@
+// console.log("hello", __dirname, __filename);
+
+// (function (require, module, exports, __filename, __dirname) {
+const obj = require("./user");
+
+console.log(obj.user);
+obj.sayHello();
+// })();
---
+++ nodeJS-basics/user.js
@@ -0,0 +1,15 @@
+const user = {
+  name: "Lera",
+  age: 20,
+};
+
+const user2 = {
+  name: "Igor",
+};
+
+module.exports = {
+  user,
+  sayHello() {
+    console.log("hello");
+  },
+};
