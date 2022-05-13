7b097b574e56c2986e42a852866603a2ecb00581
Vailery <leraliabik8@gmail.com>
2022-05-13 15:43:43 +0300

    feat: add functionality of adds courses
    show info about course

--- node-express/routes/add.js
+++ node-express/routes/add.js
@@ -1,4 +1,5 @@
 const { Router } = require("express");
+const Course = require("../models/course");
 const router = Router();
 
 router.get("/", (req, res) => {
@@ -6,6 +7,14 @@ router.get("/", (req, res) => {
     title: "Добавить курсы",
     isAdd: true,
   });
+});
+
+router.post("/", async (req, res) => {
+  const course = new Course(req.body.title, req.body.price, req.body.image);
+
+  await course.save();
+
+  res.redirect("/courses");
 });
 
 module.exports = router;
